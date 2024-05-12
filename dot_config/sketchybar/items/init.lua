18fc0e4537c0c07fa2c8661b9a504f5664417a52
lxgreen <alxgreen@gmail.com>
2024-05-13 00:33:26 +0300

    FelixKratz sketchybar setup; tmux status widgets removed

--- dot_config/sketchybar/items/init.lua
+++ dot_config/sketchybar/items/init.lua
@@ -1,11 +1,10 @@
-require("items.left.spaces.spaces")
-require("items.left.front_app")
--- replace with aerospace
-require("items.left.yabai")
-
-require("items.center.keyboard_layout")
-require("items.center.svim")
-
-require("items.right.datetime")
-require("items.right.battery")
-require("items.right.brew")
+require("items.apple")
+require("items.menus")
+require("items.spaces")
+require("items.front_app")
+require("items.calendar")
+require("items.widgets")
+require("items.media")
+require("items.svim")
+require("items.brew")
+require("items.keyboard_layout")
