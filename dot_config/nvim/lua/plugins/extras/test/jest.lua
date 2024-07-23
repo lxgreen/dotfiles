2f9ae42e3c5f366330d664f994715294ffaaeb49
lxgreen <alxgreen@gmail.com>
2024-07-23 16:09:13 +0300

    jest config fixed

--- dot_config/nvim/lua/plugins/extras/test/jest.lua
+++ dot_config/nvim/lua/plugins/extras/test/jest.lua
@@ -6,7 +6,17 @@ return {
 		table.insert(
 			opts.adapters,
 			require("neotest-jest")({
-				env = { CI = true },
+				ignore_file_types = { "markdown" },
+				jestCommand = "yarn test:unit",
+				-- TODO: this ENV should be set by per project, currently allows to run yoshi tests locally (no sled)
+				env = {
+					CI = true,
+					ARTIFACT_VERSION = "1.0.0",
+					BUILD_ID = 123456789,
+					agentType = "ci",
+					GIT_REMOTE_URL = "git@github.com:some-org/some-repo.git",
+					BUILD_VCS_NUMBER = "0123456789abcdef0123456789abcdef",
+				},
 				cwd = function()
 					return vim.fn.getcwd()
 				end,
