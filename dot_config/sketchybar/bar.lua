585a08991859a10c4c254b317caccc881018cc8c
alex greenstein <alxgreen@gmail.com>
2024-07-31 20:09:01 +0300

    gitconfig secrets; wez: 0 transparency; fzf: completions

--- dot_config/sketchybar/bar.lua
+++ dot_config/sketchybar/bar.lua
@@ -3,7 +3,7 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
 	topmost = "window",
-	height = 34,
+	height = 35,
 	color = colors.bar.bg,
 	padding_right = 2,
 	padding_left = 2,
