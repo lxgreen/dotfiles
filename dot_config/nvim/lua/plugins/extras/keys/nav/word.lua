84d3586889a0f33d7a1115a7dcaf406cfff51a60
alex greenstein <alxgreen@gmail.com>
2024-08-06 16:46:42 +0300

    nvim: fix word-wise operator pending bindings

--- dot_config/nvim/lua/plugins/extras/keys/nav/word.lua
+++ dot_config/nvim/lua/plugins/extras/keys/nav/word.lua
@@ -2,9 +2,9 @@ return {
 	"chrisgrieser/nvim-spider",
 	optional = true,
 	keys = {
-		{ "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "word-wise-w" } },
-		{ "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "word-wise-e" } },
-		{ "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "word-wise-b" } },
-		{ "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "word-wise-ge" } },
+		{ "w", "<cmd>lua require('spider').motion('w')<CR>", desc = "word-wise-w", mode = { "n", "o" } },
+		{ "e", "<cmd>lua require('spider').motion('e')<CR>", desc = "word-wise-e", mode = { "n", "o" } },
+		{ "b", "<cmd>lua require('spider').motion('b')<CR>", desc = "word-wise-b", mode = { "n", "o" } },
+		{ "ge", "<cmd>lua require('spider').motion('ge')<CR>", desc = "word-wise-ge", mode = { "n", "o" } },
 	},
 }
