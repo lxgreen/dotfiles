a68cfeb882eeef4f11f4f51e756f97eb9124cb9f
alex greenstein <alxgreen@gmail.com>
2024-08-16 00:30:59 +0300

    nvim: tmux navigation in terminal; lazygit: disable <c-*> bindings

--- dot_config/nvim/lua/plugins/extras/keys/nav/tmux.lua
+++ dot_config/nvim/lua/plugins/extras/keys/nav/tmux.lua
@@ -2,11 +2,17 @@ return {
 	"alexghergh/nvim-tmux-navigation",
 	optional = true,
 	keys = {
-		{ "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", { noremap = true, desc = "Navigate left" } },
-		{ "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", { noremap = true, desc = "Navigate down" } },
-		{ "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", { noremap = true, desc = "Navigate up" } },
-		{ "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", { noremap = true, desc = "Navigate right" } },
-		{ "<C-p>", "<cmd>NvimTmuxNavigateLastActive<cr>", { noremap = true, desc = "Navigate last active" } },
-		{ "<C-n>", "<cmd>NvimTmuxNavigateNext<cr>", { noremap = true, desc = "Navigate next" } },
+		{ "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", noremap = true, desc = "Navigate left", mode = { "n", "t" } },
+		{ "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", noremap = true, desc = "Navigate down", mode = { "n", "t" } },
+		{ "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", noremap = true, desc = "Navigate up", mode = { "n", "t" } },
+		{ "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", noremap = true, desc = "Navigate right", mode = { "n", "t" } },
+		{
+			"<C-p>",
+			"<cmd>NvimTmuxNavigateLastActive<cr>",
+			noremap = true,
+			desc = "Navigate last active",
+			mode = { "n", "t" },
+		},
+		{ "<C-n>", "<cmd>NvimTmuxNavigateNext<cr>", noremap = true, desc = "Navigate next", mode = { "n", "t" } },
 	},
 }
