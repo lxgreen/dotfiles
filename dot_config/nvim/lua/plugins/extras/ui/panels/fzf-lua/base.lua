22858472b883168752c65d02155c451537e03c6a
alex greenstein <alxgreen@gmail.com>
2024-09-14 21:06:19 +0300

    nvim: ignore-patterns in fzf-lua and telescope

--- dot_config/nvim/lua/plugins/extras/ui/panels/fzf-lua/base.lua
+++ dot_config/nvim/lua/plugins/extras/ui/panels/fzf-lua/base.lua
@@ -1,12 +1,51 @@
+local ignore_patterns = {
+	".git/",
+	"node_modules/",
+	".cache/",
+	".next/",
+	"messages_ar.json",
+	"messages_bg.json",
+	"messages_ca.json",
+	"messages_cs.json",
+	"messages_da.json",
+	"messages_de.json",
+	"messages_el.json",
+	"messages_es.json",
+	"messages_fi.json",
+	"messages_fr.json",
+	"messages_he.json",
+	"messages_hi.json",
+	"messages_hu.json",
+	"messages_id.json",
+	"messages_it.json",
+	"messages_ja.json",
+	"messages_ko.json",
+	"messages_lt.json",
+	"messages_ms.json",
+	"messages_nl.json",
+	"messages_no.json",
+	"messages_pl.json",
+	"messages_pt.json",
+	"messages_ro.json",
+	"messages_ru.json",
+	"messages_sk.json",
+	"messages_sl.json",
+	"messages_sv.json",
+	"messages_th.json",
+	"messages_tl.json",
+	"messages_tr.json",
+	"messages_uk.json",
+	"messages_vi.json",
+	"messages_zh.json",
+}
+
 return {
 	"ibhagwan/fzf-lua",
 	config = function()
 		local fzf_lua = require("fzf-lua")
 		fzf_lua.setup({
-			keys = {
-				{ "<leader><space>", false },
-				{ "<space><space>", false },
-			},
+			file_ignore_patterns = ignore_patterns,
+			fzf_colors = true,
 			winopts = {
 				on_create = function()
 					local function feedkeys(normal_key, insert_key)
