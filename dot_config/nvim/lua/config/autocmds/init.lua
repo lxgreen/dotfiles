b43bf9dc9867c90ee21195a245aec1491e5c1554
alex greenstein <alxgreen@gmail.com>
2024-09-22 18:32:59 +0300

    nvim+wezterm: open in nvim local file link

--- dot_config/nvim/lua/config/autocmds/init.lua
+++ dot_config/nvim/lua/config/autocmds/init.lua
@@ -1,5 +1,16 @@
 local AutocmdsUtil = require("config.autocmds.util")
 
+-- Save last nvim server id when nvim loses focus (FocusLost)
+-- used in Wezterm to open picked file in nvim
+vim.api.nvim_create_autocmd("FocusLost", {
+	group = vim.api.nvim_create_augroup("focus_lost", {}),
+	pattern = "*",
+	callback = function()
+		local servername = vim.v.servername
+		vim.fn.writefile({ servername }, "/tmp/nvim-focuslost")
+	end,
+})
+
 vim.api.nvim_create_autocmd("Signal", {
 	desc = "Change colorscheme after system theme changed",
 	callback = vim.schedule_wrap(AutocmdsUtil.colorscheme_update),
