df6b5f725b3635ed52b762a606f0bdfe67cba7bc
alex greenstein <alxgreen@gmail.com>
2025-01-24 22:42:50 +0200

    nvim: neovide opts; fish: init config (aimuzov)

--- dot_config/nvim/lua/config/options/init.lua
+++ dot_config/nvim/lua/config/options/init.lua
@@ -43,6 +43,17 @@ local options = {
 }
 
 vim.opt.iskeyword:append({ "!", "=", "<", ">" })
+vim.o.guifont = "JetBrains Mono:h18"
+
+-- TODO: move to separate module
+if vim.g.neovide then
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
 
 OptionsUtil.options_apply(options)
 OptionsUtil.mouse_context_menu_fix()
