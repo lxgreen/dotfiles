7c627b4e31bcb7aa03cd3205654d70cc7524c7f8
alex greenstein <alxgreen@gmail.com>
2025-03-03 22:32:00 +0200

    nvim: fix snacks scroll

--- dot_config/nvim/lua/plugins/extras/ui/editor/cursor.lua
+++ dot_config/nvim/lua/plugins/extras/ui/editor/cursor.lua
@@ -2,7 +2,24 @@ return {
 	{
 		"folke/snacks.nvim",
 		opts = {
-			scroll = { enabled = true },
+			scroll = {
+				animate = {
+					duration = { step = 15, total = 250 },
+					easing = "linear",
+				},
+				-- faster animation when repeating scroll after delay
+				animate_repeat = {
+					delay = 100, -- delay in ms before using the repeat animation
+					duration = { step = 5, total = 50 },
+					easing = "linear",
+				},
+				-- what buffers to animate
+				filter = function(buf)
+					return vim.g.snacks_scroll ~= false
+						and vim.b[buf].snacks_scroll ~= false
+						and vim.bo[buf].buftype ~= "terminal"
+				end,
+			},
 		},
 	},
 }
