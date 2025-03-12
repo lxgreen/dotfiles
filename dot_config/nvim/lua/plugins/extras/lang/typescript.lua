53a525d3236e42cd404e5514cb5d876fa35a37a6
alex greenstein <alxgreen@gmail.com>
2025-03-12 18:14:25 +0200

    nvim: run full typecheck on <leader>ck, show results in Trouble

---
+++ dot_config/nvim/lua/plugins/extras/lang/typescript.lua
@@ -0,0 +1,9 @@
+return {
+	"jellydn/typecheck.nvim",
+	dependencies = { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+	ft = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
+	opts = {
+		debug = true,
+		mode = "trouble", -- "quickfix" | "trouble"
+	},
+}
