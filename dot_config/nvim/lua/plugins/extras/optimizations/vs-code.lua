39e84e871052b4f273ec9a1ff889de045a598cb1
alex greenstein <alxgreen@gmail.com>
2025-03-19 13:43:47 +0200

    nvim, aerospace
    - nvim: disable snacks for vscode (navigation issues)
    - aerospace: window gaps consider borders width

--- dot_config/nvim/lua/plugins/extras/optimizations/vs-code.lua
+++ dot_config/nvim/lua/plugins/extras/optimizations/vs-code.lua
@@ -4,13 +4,4 @@ end
 
 return {
 	{ "folke/snacks.nvim", cond = is_not_vs_code },
-	-- { "echasnovski/mini.ai", cond = is_not_vs_code },
-	-- { "echasnovski/mini.comment", cond = is_not_vs_code },
-	-- { "echasnovski/mini.move", cond = is_not_vs_code },
-	-- { "echasnovski/mini.pairs", cond = is_not_vs_code },
-	-- { "echasnovski/mini.surround", cond = is_not_vs_code },
-	-- { "nvim-treesitter/nvim-treesitter", cond = is_not_vs_code },
-	-- { "nvim-treesitter/nvim-treesitter-textobjects", cond = is_not_vs_code },
-	-- { "JoosepAlviste/nvim-ts-context-commentstring", cond = is_not_vs_code },
-	-- { "gbprod/yanky.nvim", cond = is_not_vs_code },
 }
