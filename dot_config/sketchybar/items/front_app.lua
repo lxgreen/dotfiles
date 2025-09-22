a3632c0cf78c69b5c7a40f898d5b30f1356210a8
alex greenstein <alxgreen@gmail.com>
2025-09-22 12:50:49 +0300

    sketchybar app icons update

--- dot_config/sketchybar/items/front_app.lua
+++ dot_config/sketchybar/items/front_app.lua
@@ -1,10 +1,15 @@
 local colors = require("colors")
 local settings = require("settings")
 
+local app_icons = require("helpers.app_icons")
+
 local front_app = sbar.add("item", "front_app", {
     display = "active",
     icon = {
-        drawing = false
+        drawing = true,
+        font = "sketchybar-app-font:Regular:16.0",
+        padding_left = 3,
+        padding_right = 3
     },
     label = {
         font = {
@@ -16,9 +21,16 @@ local front_app = sbar.add("item", "front_app", {
 })
 
 front_app:subscribe("front_app_switched", function(env)
+    local app_name = env.INFO
+    local icon = app_icons[app_name] or app_icons["Default"]
+    
     front_app:set({
+        icon = {
+            string = icon,
+            font = "sketchybar-app-font:Regular:16.0"
+        },
         label = {
-            string = env.INFO
+            string = app_name
         }
     })
 end)
